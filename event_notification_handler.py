28c41ba03ba0f827a1cf9a69ef434ab70b109487
MJedr <jedrych.marcysia@gmail.com>
2022-01-11 16:05:32 +0100

    filter namespaces for notifications

--- event_notification_handler.py
+++ event_notification_handler.py
@@ -1,9 +1,11 @@
 import kopf
 import zulip
 
+ACCEPTED_NAMESPACES = ["inspire-prod", "inspire-qa"]
+
 client = zulip.Client()
 
-request = {
+zulip_request_payload = {
     "type": "stream",
     "to": "INSPIRE",
 }
@@ -12,14 +14,16 @@ request = {
 @kopf.on.event("", "v1", "pods")
 def event_notification_handler(event, **_):
     object_container_status = event["object"]["status"].get("containerStatuses")[0]
+    object_metadata = event["object"]["metadata"]
     if not object_container_status:
+        return
+    if object_metadata["namespace"] not in ACCEPTED_NAMESPACES:
         return
     if (
         "terminated" in object_container_status["state"]
         and object_container_status["state"]["terminated"]["reason"] == "Error"
     ):
-        object_metadata = event["object"]["metadata"]
-        request["topic"] = object_metadata["namespace"]
+        zulip_request_payload["topic"] = object_metadata["namespace"]
         resource_kind = object_metadata["ownerReferences"][0]["kind"]
         resource_name = object_metadata["ownerReferences"][0]["name"]
         error_message = object_container_status["state"]["terminated"].get("message")
@@ -29,5 +33,5 @@ def event_notification_handler(event, **_):
             else f":skull_and_crossbones: {resource_kind} **{resource_name}** failed."
         )
 
-        request["content"] = zulip_message_content
-        client.send_message(request)
+        zulip_request_payload["content"] = zulip_message_content
+        client.send_message(zulip_request_payload)
