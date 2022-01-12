a7b92109ed96b80f32046cec5198e599b504bc1d
MJedr <jedrych.marcysia@gmail.com>
2022-01-12 11:46:50 +0100

    update filtering

--- event_notification_handler.py
+++ event_notification_handler.py
@@ -18,27 +18,21 @@ def configure(settings: kopf.OperatorSettings, **_):
     settings.posting.level = logging.ERROR
 
 
-@kopf.on.event("", "v1", "pods")
+@kopf.on.event("", "v1", "pods", field='kind', value='Job')
+@kopf.on.event("", "v1", "pods", field='status.phase', value='Failed')
 def event_notification_handler(event, **_):
-    object_container_status = event["object"]["status"].get("containerStatuses")[0]
-    object_metadata = event["object"]["metadata"]
+    object_container_status = event["object"]["status"].get("containerStatuses")
     if not object_container_status:
         return
-    if object_metadata["namespace"] not in ACCEPTED_NAMESPACES:
-        return
-    if (
-        "terminated" in object_container_status["state"]
-        and object_container_status["state"]["terminated"]["reason"] == "Error"
-    ):
-        zulip_request_payload["topic"] = object_metadata["namespace"]
-        resource_kind = object_metadata["ownerReferences"][0]["kind"]
-        resource_name = object_metadata["ownerReferences"][0]["name"]
-        error_message = object_container_status["state"]["terminated"].get("message")
-        zulip_message_content = (
-            f":skull_and_crossbones: {resource_kind} **{resource_name}** failed with the following error:\n> {error_message}."
-            if error_message
-            else f":skull_and_crossbones: {resource_kind} **{resource_name}** failed."
-        )
-
-        zulip_request_payload["content"] = zulip_message_content
-        client.send_message(zulip_request_payload)
+    object_metadata = event["object"]["metadata"]
+    zulip_request_payload["topic"] = object_metadata["namespace"]
+    resource_kind = object_metadata["ownerReferences"][0]["kind"]
+    resource_name = object_metadata["ownerReferences"][0]["name"]
+    error_message = object_container_status[0]["state"]["terminated"].get("message")
+    zulip_message_content = (
+        f":skull_and_crossbones: {resource_kind} **{resource_name}** failed with the following error:\n> {error_message}."
+        if error_message
+        else f":skull_and_crossbones: {resource_kind} **{resource_name}** failed."
+    )
+    zulip_request_payload["content"] = zulip_message_content
+    client.send_message(zulip_request_payload)
