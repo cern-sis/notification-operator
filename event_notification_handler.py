7bfc3147d3aecd4146bfb3c50103611aca591478
MJedr <jedrych.marcysia@gmail.com>
2022-09-22 11:06:35 +0200

    add notification handler for pods

--- event_notification_handler.py
+++ event_notification_handler.py
@@ -35,3 +35,19 @@ def event_notification_handler(old, new, status, namespace, **kwargs):
     zulip_message_content = f":skull_and_crossbones: {resource_kind} **{resource_name}** failed with the following error:\n> {error_reason}: {error_message}."
     zulip_request_payload["content"] = zulip_message_content
     client.send_message(zulip_request_payload)
+
+
+@kopf.on.field("v1", "pod", field="status.conditions")
+def pod_events_notification_handler(old, new, status, namespace, **kwargs):
+    if namespace not in ACCEPTED_NAMESPACES:
+        return
+    data = kwargs["body"]
+    zulip_request_payload["to"] = "test"
+    zulip_request_payload["topic"] = namespace
+    resource_kind = data["kind"]
+    resource_name = data["metadata"]["name"]
+    error_reason = status["conditions"][0]["reason"]
+    error_message = status["conditions"][0]["message"]
+    zulip_message_content = f":skull_and_crossbones: {resource_kind} **{resource_name}** failed with the following error:\n> {error_reason}: {error_message}."
+    zulip_request_payload["content"] = zulip_message_content
+    client.send_message(zulip_request_payload)
