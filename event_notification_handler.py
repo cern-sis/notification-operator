d85195fb4a8bd317f530e76ef509315d37a5ff0b
MJedr <jedrych.marcysia@gmail.com>
2022-10-04 16:29:34 +0200

    add handler for pod events and refactor code

--- event_notification_handler.py
+++ event_notification_handler.py
@@ -9,38 +9,31 @@ client = zulip.Client()
 zulip_request_payload = {"type": "stream"}
 
 
-@kopf.on.startup()
-def configure(settings: kopf.OperatorSettings, **_):
-    settings.posting.level = logging.ERROR
+class NotificationHandler:
+    def prepare_message(self, old, new, status, namespace, **kwargs):
+        if namespace not in os.environ.get("ACCEPTED_NAMESPACES").split(','):
+            return
+        data = kwargs["body"]
+        zulip_request_payload["to"] = "test"
+        zulip_request_payload["topic"] = namespace
+        resource_kind = data["kind"]
+        resource_name = data["metadata"]["name"]
+        error_reason = status["conditions"][0]["reason"]
+        error_message = status["conditions"][0]["message"]
+        zulip_message_content = f":skull_and_crossbones: {resource_kind} **{resource_name}** failed with the following error:\n> {error_reason}: {error_message}."
+        zulip_request_payload["content"] = zulip_message_content
+        client.send_message(zulip_request_payload)
+
+    def configure(settings: kopf.OperatorSettings, **_):
+        settings.posting.level = logging.ERROR
 
 
-@kopf.on.field("batch", "v1", "jobs", field="status.conditions")
-def event_notification_handler(old, new, status, namespace, **kwargs):
-    if namespace not in os.environ.get("ACCEPTED_NAMESPACES").split(','):
-        return
-    data = kwargs["body"]
-    zulip_request_payload["to"] = namespace.split("-")[0]
-    zulip_request_payload["topic"] = namespace
-    resource_kind = data["kind"]
-    resource_name = data["metadata"]["name"]
-    error_reason = status["conditions"][0]["reason"]
-    error_message = status["conditions"][0]["message"]
-    zulip_message_content = f":skull_and_crossbones: {resource_kind} **{resource_name}** failed with the following error:\n> {error_reason}: {error_message}."
-    zulip_request_payload["content"] = zulip_message_content
-    client.send_message(zulip_request_payload)
+notification_handler = NotificationHandler()
 
-
-@kopf.on.field("v1", "pod", field="status.conditions")
-def pod_events_notification_handler(old, new, status, namespace, **kwargs):
-    if namespace not in ACCEPTED_NAMESPACES:
-        return
-    data = kwargs["body"]
-    zulip_request_payload["to"] = "test"
-    zulip_request_payload["topic"] = namespace
-    resource_kind = data["kind"]
-    resource_name = data["metadata"]["name"]
-    error_reason = status["conditions"][0]["reason"]
-    error_message = status["conditions"][0]["message"]
-    zulip_message_content = f":skull_and_crossbones: {resource_kind} **{resource_name}** failed with the following error:\n> {error_reason}: {error_message}."
-    zulip_request_payload["content"] = zulip_message_content
-    client.send_message(zulip_request_payload)
+kopf.on.startup()(notification_handler.prepare_message)
+kopf.on.field("batch", "v1", "jobs", field="status.conditions")(
+    notification_handler.prepare_message
+)
+kopf.on.field("v1", "pod", field="status.conditions")(
+    notification_handler.prepare_message
+)
