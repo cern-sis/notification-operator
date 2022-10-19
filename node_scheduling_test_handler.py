2d77b2c05c655eb411af6dffbaa3b7cfb0c822e4
Ben <benjamin-bergia@users.noreply.github.com>
2022-10-19 16:34:42 +0200

    Don't fire errors as kubernetes events

--- node_scheduling_test_handler.py
+++ node_scheduling_test_handler.py
@@ -9,6 +9,11 @@ POD_CREATION_TIMEOUT = float(os.environ.get("POD_CREATION_TIMEOUT", 5.0))
 POD_CREATION_INTERVAL = float(os.environ.get("POD_CREATION_INTERVAL", 30.0))
 
 
+@kopf.on.startup()
+def configure(settings: kopf.OperatorSettings, **_):
+    settings.posting.enabled = False
+
+
 @kopf.timer("node",
             interval=POD_CREATION_INTERVAL,
             sharp=True,
