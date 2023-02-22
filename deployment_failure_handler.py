119700a43939cc7491499e77bf5e27c03ab97217
jimil749 <jimildesai42@gmail.com>
2023-02-22 15:49:59 +0100

    run pre-commit
    Signed-off-by: jimil749 <jimildesai42@gmail.com>

--- deployment_failure_handler.py
+++ deployment_failure_handler.py
@@ -1,12 +1,11 @@
-import logging
 import os
 
 import kopf
 import zulip
-from kubernetes import client, config, watch
+from kubernetes import client, config
 
 config.load_incluster_config()
-v1=client.AppsV1Api()
+v1 = client.AppsV1Api()
 
 client = zulip.Client()
 
@@ -16,7 +15,7 @@ def _prepare_message_for_deployment(replicas_unavailable, deployment, **kwargs):
     if namespace not in os.environ.get("ACCEPTED_NAMESPACES", "").split(","):
         return
     deployment_name = deployment.metadata.name
-    zulip_msg_content = f":double_exclamation: Detected deployment **{deployment_name}** is failing. \n\n{replicas_unavailable} replica(s) is/are unavailable"
+    zulip_msg_content = f":double_exclamation: Detected deployment **{deployment_name}** is failing.\n\n{replicas_unavailable} replica(s) is/are unavailable"  # noqa
     zulip_request_payload = {
         "type": "stream",
         "to": namespace.split("-")[0],
@@ -30,12 +29,16 @@ def _prepare_message_for_deployment(replicas_unavailable, deployment, **kwargs):
 def configure(settings: kopf.OperatorSettings, **_):
     settings.posting.enabled = False
 
+
+# send notification if the deployment is failing
 @kopf.on.field("apps", "v1", "deployments", field="status.replicas")
 def pod_phase_notification_handler(old, new, body, **kwargs):
     if not old:
         return
-    deployment = v1.read_namespaced_deployment(name=body['metadata']['name'], namespace=body['metadata']['namespace'])
+    deployment = v1.read_namespaced_deployment(
+        name=body["metadata"]["name"], namespace=body["metadata"]["namespace"]
+    )
     max_unavailable = deployment.spec.strategy.rolling_update.max_unavailable
-    replicas_unavailable = (old-new)
+    replicas_unavailable = old - new
     if replicas_unavailable > max_unavailable:
         _prepare_message_for_deployment(replicas_unavailable, deployment, **kwargs)
